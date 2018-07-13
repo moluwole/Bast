f302caec02adb8ded914bbfb1555ea1e28610543
Majiyagbe Oluwole <oluwole564@gmail.com>
2018-07-13 01:59:49 +0100

    Added option for static files

--- bast/view.py
+++ bast/view.py
@@ -2,17 +2,30 @@ from jinja2 import Environment, FileSystemLoader, TemplateNotFound, PackageLoade
 import os
 
 
+def script(js_file):
+    if 'http' in js_file:
+        return '<script type="text/javascript" src="' + js_file + '"></script'
+    static_folder = os.environ['STATIC_FILES']
+    return '<script type="text/javascript" src="' + str(static_folder) + '/' + js_file + '"></script>'
+
+
+def css(css_file):
+    if 'http' in css_file:
+        return '<link rel="stylesheet" href="' + css_file + '">'
+    static_folder = os.environ['STATIC_FILES']
+    return '<link rel="stylesheet" href="' + str(static_folder) + '/' + css_file + '">'
+
+
 class TemplateRendering:
+
     def render_template(self, template_name, kwargs):
-        # template_dir = []
-        # if self.settings.get('template_path', ''):
-        #     template_dir.append(self.settings["template_path"])
         template_dir = os.environ['TEMPLATE_FOLDER']
 
-        # app_name = app.config['APP_NAME']
-        app_name = os.environ['APP_NAME']
-        # env = Environment(loader=PackageLoader('bast', 'public'), autoescape=select_autoescape(['html']))
         env = Environment(loader=FileSystemLoader(template_dir))
+
+        env.globals['css'] = css
+        env.globals['script'] = script
+
         try:
             template = env.get_template(template_name)
         except TemplateNotFound:

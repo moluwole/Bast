d3f2e2c9d966489499fb75fd56771cd4c4341ba4
Majiyagbe Oluwole <oluwole564@gmail.com>
2018-07-14 13:37:41 +0100

    Continuous Integration

--- example/app/controller/test_controller.py
+++ example/app/controller/test_controller.py
@@ -12,7 +12,10 @@ class Mine(Controller):
 
         request_data = self.only(['username', 'pass'])
         is_hash = Hash.compare(request_data['pass'], '$2b$12$7h930ePUlcqCqJfnrJCCq.bbyjmonKaz4Xp4AiQVq8s9FlijGO.se')
+
         data = {'data': 'Got this from the index method', 'username': request_data['username'],
                 'pass': Hash.encrypt(request_data['pass']), 'header': "From Temp", 'is_hash': is_hash}
         # self.json(data)
         self.view('index.html', data)
+
+

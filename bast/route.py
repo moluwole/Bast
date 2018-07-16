91badb8a9ac87f98439b77dd6b6a6c7f51f29ab0
Majiyagbe Oluwole <oluwole564@gmail.com>
2018-07-16 14:22:06 +0100

    Add functions for PUT and DELETE requests

--- bast/route.py
+++ bast/route.py
@@ -10,5 +10,11 @@ class Route:
     def post(self, url, controller_name, method_name):
         self.url.append((url, controller_name, dict(method=method_name)))
 
+    def put(self, url, controller_name, method_name):
+        self.url.append((url, controller_name, dict(method=method_name)))
+
+    def delete(self, url, controller_name, method_name):
+        self.url.append((url, controller_name, dict(method=method_name)))
+
     def show(self):
         return self.url

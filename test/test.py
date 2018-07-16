160774ce7318b21fe3cf33ab34390c574b7d4b66
Majiyagbe Oluwole <oluwole564@gmail.com>
2018-07-16 14:48:20 +0100

    Update Test for local static files

--- test/test.py
+++ test/test.py
@@ -26,30 +26,40 @@ class Test(unittest.TestCase):
     def tearDown(self):
         os.environ.clear()
 
-    # def test_hash(self):
-    #     from bast.hash import Hash
-    #     password = 'bast test password'
-    #     hashed = Hash.encrypt(password)
-    #     print(hashed)
-    #     self.assertTrue(isinstance(hashed, str))
-
-    def test_compare(self):
+    def test_hash(self):
         from bast.hash import Hash
-        password = "basrtest".encode('utf-8')
+        password = 'basttestpassword'
         hashed = Hash.encrypt(password)
+        return hashed
 
-        print(hashed, password)
-        self.assertEqual(Hash.compare(password, hashed), True)
+    # def test_compare(self):
+    #     from bast.hash import Hash
+    #     password = "basrtest".encode('utf-8')
+    #     hashed = Hash.encrypt(password)
+    #
+    #     print(hashed, password)
+    #     self.assertEqual(Hash.compare(password, hashed), True)
 
-    def test_script(self):
+    def test_script_server(self):
         from bast.view import script
-        script_link = script("http://bootstrap.css")
-        self.assertEqual('<script type="text/javascript" src="http://bootstrap.css"></script>', script_link, "Not equal")
+        script_link = script("http://bootstrap.js")
+        self.assertEqual('<script type="text/javascript" src="http://bootstrap.js"></script>', script_link, "Not equal")
 
-    def test_css(self):
+    def test_css_server(self):
         from bast.view import css
         css_link = css('http://mycssfile.com')
         self.assertEqual('<link rel="stylesheet" href="http://mycssfile.com">', css_link)
+
+    def test_css_local(self):
+        from bast.view import css
+        css_link = css('mycssfile.css')
+        self.assertEqual('<link rel="stylesheet" href="/css/mycssfile.css">', css_link)
+
+    def test_script_local(self):
+        from bast.view import script
+        script_link = script("myscript.js")
+        self.assertEqual('<script type="text/javascript" src="/script/myscript.js"></script>', script_link,
+                         "Not equal")
 
     # def test_run_server(self):
     #     from bast.route import Route

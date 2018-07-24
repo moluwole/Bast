a4a72ef08fb8b5b4ac1f1e535fc8db89723bd744
Majiyagbe Oluwole <oluwole564@gmail.com>
2018-07-25 00:00:19 +0100

    Models and Migration (Eloquent)

--- bast/__init__.py
+++ bast/__init__.py
@@ -5,31 +5,13 @@
     For full copyright and license information, view the LICENSE distributed with the Source Code
 """
 
-import argparse
-
 from .bast import Bast
 from .controller import Controller
 from .hash import Hash
+from .migration import CreateMigration
 from .migration import Migration
+from .model import Models
 from .route import Route
 
 __version__ = "1.0"
-
-
-def bast():
-    parser = argparse.ArgumentParser(
-        description='The Command Line Parser for the Bast Framework'
-    )
-    parser.add_argument(
-        '-v', '--version',
-        action='version',
-        version='Bast Framework v%s' % __version__
-    )
-    parser.add_argument('-g', '--g',
-                        help='Generate Migration File: panther -g create:migration name',
-                        nargs='*')
-
-    args = parser.parse_args()
-    database = Migration()
-    return database.generate(args)
 

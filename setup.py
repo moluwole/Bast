2aece63e985562dac1ca1f2bde116fc5f4e7e532
Majiyagbe Oluwole <oluwole564@gmail.com>
2018-08-01 09:24:35 +0100

    Update

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def readme():
 
 
 setup(name='Bast',
-      version='0.0.1.0.5',
+      version='0.0.1.0.6',
       description='Simple yet Elegant Web Framework with MVC Patterns',
       long_description=readme(),
       classifiers=[
